93b91b83e2110a6972e35f5b4de5cd2649e8ed9a
Shun Lyu <Shun.Lyu@lincolnuni.ac.nz>
2023-10-09 22:44:51 +0800

    Task - Driver's run details - Added the basic structure of the feature.
    Finalise display content for Driveer's run details page
    
    Signed-off-by: Shun Lyu <Shun.Lyu@lincolnuni.ac.nz>

--- BRMM_webapp/app.py
+++ BRMM_webapp/app.py
@@ -65,13 +65,43 @@ def listdrivers():
     return render_template("driverlist.html", driver_list=driverList)
 
 
-@app.route("/driversrundetails")
+@app.route('/driversrundetails/<int:driver_id>', methods=['GET'])
+def driversrundetailspage(driver_id):
+    connection = getCursor()
+    connection.execute("SELECT * FROM driver;")
+    driverList = connection.fetchall()
+
+
+    """
+    TODO: Might be better to handle time calculation etc in python instead of using SQL or frontend.
+    """
+    # Fetch driver's details based on the driver_id parameter
+    connection.execute(f"SELECT md.driver_id, md.surname, md.first_name, ms.name, mr.run_num, mc.model, "
+                       f"mc.drive_class, mr.cones, mr.wd, mr.seconds FROM motorkhana.driver as md "
+                       f"LEFT JOIN motorkhana.car as mc ON mc.car_num = md.car "
+                       f"LEFT JOIN motorkhana.run as mr ON mr.dr_id = md.driver_id "
+                       f"LEFT JOIN motorkhana.course as ms ON mr.crs_id = ms.course_id "
+                       f"WHERE md.driver_id = %s;", (driver_id,))
+    selected_driver_details = connection.fetchall()
+
+    return render_template('driversrundetails.html', driver_list=driverList, selected_driver=selected_driver_details)
+
+
+@app.route('/driversrundetails', methods=['GET', 'POST'])
 def driversrundetails():
     connection = getCursor()
     connection.execute("SELECT * FROM driver;")
     driverList = connection.fetchall()
-    print(driverList)
-    return render_template("driversrundetails.html", driver_list=driverList)
+
+    if request.method == 'POST':
+        # Handle the form submission here
+        selected_driver_id = request.form.get('selected_driver')
+        if selected_driver_id:
+            # Construct the URL for the individual run details page
+            url = f"/driversrundetails/{selected_driver_id}"
+            return redirect(url)
+
+    return render_template('driversrundetails.html', driver_list=driverList)
 
 
 @app.route("/listcourses")
